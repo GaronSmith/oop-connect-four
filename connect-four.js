57f79eb008b6eada066cbf30b526fd4409057792
Antonio <anguilar@aol.com>
2020-11-06 13:16:06 -0800

    Added a start new game button

--- connect-four.js
+++ connect-four.js
@@ -1,6 +1,7 @@
 // TO-DO
 import { Game } from "./game.js"
 
+let currentPlayer = 'red'
 
 let GAME = undefined
 
@@ -15,48 +16,23 @@ const updateUI = () => {
         boardHolder.classList.remove('is-invisible')
         gameName.innerHTML = GAME.getName()
     }
+
+    if(GAME.currentPlayer === 1) {
+        currentPlayer = "red"
+    } else {
+        currentPlayer = "black"
+    }
+
 }
 window.addEventListener("DOMContentLoaded", event => {
-/*
-TODO
 
-[*] create counter
-[*] create column object
-
-[*] add eventlistener to show whose turn
-
-[*]create function to take in the column clicked and object counter
-   this then creates a new div
-   set attribute of new div to token color
-   append to clicked square
+// let counter = 1;
 
 
-*/
-
-
-
-
-
-
-let counter = 1;
 
 let columns = {};
 
-let currentPlayer = 'red'
-
-const changePlayer = count => {
-    if (count % 2 === 0){
-        currentPlayer = 'black'
-    } else {
-        currentPlayer = 'red'
-    }
-}
-
-
-// event listener hover
-
 const columnHeaders = document.getElementById('click-targets')
-
  columnHeaders.addEventListener('mouseover', event => {
     if (event.target.id !== 'click-targets') {
         document.getElementById(event.target.id).classList.add('token', currentPlayer)
@@ -68,35 +44,41 @@ const columnHeaders = document.getElementById('click-targets')
     }
  })
 
+ columnHeaders.addEventListener("click", event => {
 
-columnHeaders.addEventListener('click', event =>{
-    let click = event.target.id
+    console.log(currentPlayer)
+    GAME.playInColumn()
+    updateUI()
+
+ })
+// columnHeaders.addEventListener('click', event =>{
+//     let click = event.target.id
 
 
-    if (columns[click]){
-        columns[click] -= 1;
-    } else {
-        columns[click] = 5
-    }
+//     if (columns[click]){
+//         columns[click] -= 1;
+//     } else {
+//         columns[click] = 5
+//     }
 
-    if(columns[click] === 0) {
-        document.getElementById(click).classList.add('full')
-    }
-    let columnClick = click.split('-')[1]
-    let row = columns[click]
+//     if(columns[click] === 0) {
+//         document.getElementById(click).classList.add('full')
+//     }
+//     let columnClick = click.split('-')[1]
+//     let row = columns[click]
 
-    let position = document.createElement('div');
-    position.classList.add('token', currentPlayer)
+//     let position = document.createElement('div');
+//     position.classList.add('token', currentPlayer)
 
-    let nextSquare = document.getElementById(`square-${row}-${columnClick}`)
-    nextSquare.appendChild(position)
+//     let nextSquare = document.getElementById(`square-${row}-${columnClick}`)
+//     nextSquare.appendChild(position)
 
-    counter++
+//     counter++
 
-    let oldColor = currentPlayer
-    changePlayer(counter)
-    document.getElementById(click).classList.replace(oldColor, currentPlayer)
-})
+//     let oldColor = currentPlayer
+//     changePlayer(counter)
+//     document.getElementById(click).classList.replace(oldColor, currentPlayer)
+// })
 
 let playerOneText = document.getElementById("player-1-name")
 let playerTwoText = document.getElementById("player-2-name")
